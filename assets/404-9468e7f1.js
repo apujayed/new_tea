0b8654748c0043efb027d7943c69d8b24e8a120a
Jayed Apu <95185551+apujayed@users.noreply.github.com>
2023-09-07 02:04:24 +0600

    first commit

---
+++ assets/404-9468e7f1.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-ecc2ffdd.js";function t(){return o.useEffect(()=>{window.location.replace("/dashboard")},[]),null}export{t as default};
--- assets/404-a711e3ab.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-36de2636.js";function t(){return o.useEffect(()=>{window.location.replace("/dashboard")},[]),null}export{t as default};
